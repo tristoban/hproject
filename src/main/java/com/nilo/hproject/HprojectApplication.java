5fa7e90d8279bbc0336b0a88ad426e85098409b9
Matias Ponce de León <matiasponcedeleon@Air-de-Matias.fibertel.com.ar>
2024-11-30 20:23:52 -0300

    Primer Commit

---
+++ src/main/java/com/nilo/hproject/HprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nilo.hproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nilo/hproject/HprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilo.hproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
